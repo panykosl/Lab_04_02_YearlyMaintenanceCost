6e39e8670c6603ccbb4ed3f513de7780f3b5ff02
First Commit <panykosl@mail.uc.edu>
2023-09-14 13:47:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 30.0;
+        double summerCost = 35.0;
+        double fallCost = 28.0;
+        double winterCost = 40.0;
+        double totalCost = 0.0;
+        totalCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The spring cost is " + springCost + " plus the summer cost " + summerCost + " plus the fall cost " + fallCost + " plus the winter cost" + winterCost + " equals " +totalCost);
+    }
+}
